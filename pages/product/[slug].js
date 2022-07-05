8bf239c1fd1318e75306000c048c08f765018a64
jcromerohdz <jcromerohdz@gmail.com>
2022-07-05 09:12:53 -0700

    Shopping Cart - CartScreen component to create a cart and add and remove items of the cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,6 +8,9 @@ import { Store } from '../../utils/store'
 
 export default function ProductScreen() {
   const { state, dispatch } = useContext(Store)
+  
+  const router = useRouter()
+  
   const {query} = useRouter()
   const {slug} = query
   const product = data.products.find((x) => x.slug === slug )
@@ -25,6 +28,7 @@ export default function ProductScreen() {
     }
 
     dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity}})
+    router.push('/cart')
   }
 
   return (
