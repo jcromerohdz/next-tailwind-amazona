9fa1adf273da7e48b3bb85d55a2d3d9b6e6004ef
jcromerohdz <jcromerohdz@gmail.com>
2022-07-05 09:34:41 -0700

    Shopping Cart - Implementing Update Quantity in the CartScreen component

--- pages/cart.js
+++ pages/cart.js
@@ -16,6 +16,11 @@ export default function CartScreen() {
     dispatch({ type: 'CART_REMOVE_ITEM', payload: item });
   };
 
+  const updateCartHandler = (item, qty) => {
+    const quantity = Number(qty)
+    dispatch({type:'CART_ADD_ITEM', payload:{...item, quantity}})
+  }
+
   return (
     <Layout title="Shopping Cart">
       <h1 className="mb-4 text-xl">Shopping Cart</h1>
@@ -54,7 +59,13 @@ export default function CartScreen() {
                         </Link>
                       </td>
                       <td className="p-5 text-right">
-                        {item.quantity}
+                        <select value={item.quantity} onChange={(e) => updateCartHandler(item, e.target.value)}>
+                          {
+                            [...Array(item.countInStock).keys()].map(x => (
+                              <option key={x+1} value={x+1}>{x+1}</option>
+                            ))
+                          }
+                        </select>
                       </td>
                       <td className="p-5 text-right">
                         ${item.price}
@@ -94,7 +105,6 @@ export default function CartScreen() {
             
           </div>
         )
-
       }
     </Layout>
   )
